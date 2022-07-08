1eab5da0dc2a91afcca322bcd7df64d22dc553db
Hardbass1991 <midailsorridente@gmail.com>
2022-07-08 10:11:44 -0700

    second version of printf, yields seg fault

--- _printf.c
+++ _printf.c
@@ -2,57 +2,75 @@
 #include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 /**
  * print_all - prints everyargument passe according to input format string
  * @format: input format string
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, slen, perc = 0;
+	int i = 0, slen, perc = 0, n = 0;
 	va_list ap;
 	char *tmp;
 
 	va_start(ap, format);
 	while (format && format[i])
 	{
-		if (i = 0)
+		if (i == 0)
 		{
 			if (format[i] != '%')
-				write(1, format[i], 1);
+			{
+				tmp[0] = format[i];
+				n += 1;
+				write(1, tmp, 1);
+			}
 			else
 				perc += 1;
 		}
 		else
 		{
+			printf("p1");
 			if (format[i - 1] == '%')
 			{	
 				switch (format[i])
 				{
 					case 'c':
-						write(1, va_arg(ap, int), 1);
+						tmp[0] = va_arg(ap, int);
+						n += 1;
+						write(1, tmp, 1);
 						break;
 					case 's':
 						tmp = va_arg(ap, char*);
 						slen = strlen(tmp);
+						n += 1;
 						write(1, tmp, slen);
 						break;
 					case '%':
 						perc += 1;
 						if (perc % 2 == 0)
-							write(1, '%', 1);
+						{
+							tmp = "%";
+							n += 1;
+							write(1, tmp, 1);
+						}
+						break;
 					default:
-						write(1, format[i], 1);
+						tmp[0] = format[i];
+						n += 1;
+						write(1, tmp, 1);
+						break;
 				}
 			}
 			else
 			{
-				if (format[i] == '%')
-					continue;
-				write(1, format[i], 1);
+				tmp[0] = format[i];
+				n += 1;	
+				write(1, tmp, 1);
 			}
 		}
 		i++;
 	}
 	va_end(ap);
 	printf("\n");
+	return (n);
 }
